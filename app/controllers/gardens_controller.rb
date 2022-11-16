cfcadaaad0b0df8ca6fb86a09e6aed1212872a9f
pbragamiranda <pbragamiranda@gmail.com>
2022-11-16 18:16:50 +0100

    adding nested forms

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -8,6 +8,8 @@ class GardensController < ApplicationController
 
   # GET /gardens/1
   def show
+    # @garden = Garden.find(params[:id])
+    @plant = Plant.new
   end
 
   # GET /gardens/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :gardens
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :gardens do
+    resources :plants, only: [ :create ]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :plants, only: [ :destroy ]
 end
