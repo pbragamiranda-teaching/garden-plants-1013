b0928759c49b9c021acd082aa301f2c08bdeb395
pbragamiranda <pbragamiranda@gmail.com>
2022-11-16 17:28:03 +0100

    rails new + front edn setup + models

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Garden.destroy_all
+
+Garden.create(name: "Batch 1013",
+              banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg")
+
+Garden.create(name : "Pedro's house",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg")
